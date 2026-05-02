25b9e04eca574a7a710aade94f4f52d8c97286d9
BaimoQilin <admin@baimoqilin.top>
2026-05-02 18:52:41 +0800

    Prepare release 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.baimoqilin.aether"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
